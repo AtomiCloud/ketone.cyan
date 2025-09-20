feea7a3bb087d0df100abfcb552d5152b51a08ee
adelphi-liong <adelphi@atomi.cloud>
2025-09-20 13:57:44 +0800

    fix: default lambda

--- templates/cyan/index.ts
+++ templates/cyan/index.ts
@@ -1,12 +1,29 @@
-import { StartTemplateWithLambda } from '@atomicloud/cyan-sdk';
+import { GlobType, StartTemplateWithLambda } from '@atomicloud/cyan-sdk';
 
 StartTemplateWithLambda(async (i, d) => {
+
+
+  const vars = {};
   return {
     processors: [
       {
         name: 'cyan/default',
-        files: ['template/**/*.*'],
-        config: {},
+        files: [
+          {
+            root: "templates",
+            glob: '**/*.*',
+            type: GlobType.Template,
+            exclude: [],
+          }
+        ],
+        config: {
+          vars,
+          parser: {
+            varSyntax: [
+              ['let___', '___']
+            ],
+          },
+        },
       },
     ],
     plugins: [],
